91b145e68a4c32fefee9a45e3d0b2d1b58e17a83
ANKIT SIROHIWAL <you@example.com>
2018-04-01 02:36:54 +0530

    first commit

---
+++ stylejs.js
@@ -0,0 +1,14 @@
+
+
+function f1(){
+
+	var a=document.getElementById("piku").value;
+	console.log(a);
+	//console.log(a[0].length);
+	
+	if(a.length < 8)
+	{
+		alert("noooooooo");
+		return false;
+	}
+}
